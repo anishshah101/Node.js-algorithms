077e5ba37cdcade67ce43b0b888391ed5df95a35
Anish Shah <anishshah101@gmail.com>
2014-11-28 21:00:41 +0530

    Algo complete

--- Party/factorial.js
+++ Party/factorial.js
@@ -4,42 +4,60 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', function (chunk) {
 
 	var lines = chunk.split('\n');
-	var ind= 0;
-	var space= lines[ind].search(/ [0-9]+/);
+	var ind= 1;
+	var space= lines[ind-1].search(/ [0-9]+/);
 	
 	var temp1=parseInt(lines[0].substring(0,space));
 	var temp2=parseInt(lines[0].substring(space+1));
 	
-	while(temp1 !=0 && temp2 != 0)
+	while(temp1!=0)
 	{
-		newind=calc(lines, ind+1);
-	};
+		calc(lines, ind);
+		ind=ind+temp2+2;
+		temp1=parseInt(lines[ind-1].substring(0,space));
+		temp2=parseInt(lines[ind-1].substring(space+1));
+		if(temp1!=0)
+			process.stdout.write('\n');
+	}
 });
  
 function calc(lines, ind){
-	var space= lines[ind].search(/ [0-9]+/);
-	var temp1=parseInt(lines[ind].substring(0,space));
-	var temp2=parseInt(lines[ind].substring(space+1));
-	var A[temp2], B[temp2], C[temp2];
+	
+	var space= lines[ind-1].search(/ [0-9]+/);
+	var temp1=parseInt(lines[ind-1].substring(0,space));
+	var temp2=parseInt(lines[ind-1].substring(space+1));
+	var A=[], B=[], C=[];
 	for (var j=ind;j<ind+temp2;j++)
 	{
-		space=lines[j+1].search(/ [0-9]+/);
-		A[j]=parseInt(lines[ind].substring(0,space));
-		B[j]=parseInt(lines[ind].substring(0,space+1));
-		C[j]=A[j]/B[j];
+		space=lines[j-1].search(/ [0-9]+/);
+		A[j-1]=parseInt(lines[j].substring(0,space));
+		B[j-1]=parseInt(lines[j].substring(space+1));
+		C[j-1]=B[j-1]/A[j-1];
 	}
+	
 	for (var i=ind+temp2;i>=ind;i--)
 		{
 			var tempC=0, tempB=0, tempA=0;
 			for (var j=ind;j<=i;j++)
 			{
-				if(C[j]>C[j+1])
+				if(C[j-1]>C[j])
 				{
-					tempC=C[j], tempB=B[j], tempA=A[j];
-					C[j]=C[j+1], B[j]=B[j+1], A[j]=A[j+1];
-					C[j+1]=tempC, B[j+1]=tempB, A[j+1]=tempA;
+					tempC=C[j-1], tempB=B[j-1], tempA=A[j-1];
+					C[j-1]=C[j], B[j-1]=B[j], A[j-1]=A[j];
+					C[j]=tempC, B[j]=tempB, A[j]=tempA;
 				}
 			}	
 		}	//sorting the arrays according to values of C[j]
+		
+	var sumE=0;
+	var sumF=0;
+	for (var i=ind+temp2;i>=ind;i--)
+	{
+		if (sumE + A[i-1] <=50)
+		{
+			sumE=sumE+A[i-1];
+			sumF=sumF+B[i-1];
+		}	
+	}
+	process.stdout.write(sumE+' '+sumF);
 }
- 
